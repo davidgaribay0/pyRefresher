88739e5c47b71e1aca7032c82fb17a9859aadb82
davidgaribay0 <dm1garibay@gmail.com>
2023-06-15 14:22:07 -0400

    valid subsequence

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from algorithms.validate_subsequence import is_valid_subsequence
+
+
 def main():
-    pass
+    print(is_valid_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 10]))
+
 
 if __name__ == "__main__":
     main()
